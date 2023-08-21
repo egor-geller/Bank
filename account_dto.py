408bd7a1f38841b1e9944b994b0cfc9e10d5a715
Egor Geller <egkingers11@gmail.com>
2023-08-21 22:33:33 +0300

    Added equals and hash

--- account_dto.py
+++ account_dto.py
@@ -29,9 +29,20 @@ class AccountDto:
     def set_balance(self, balance):
         self.__balance = balance
 
+    def __eq__(self, other):
+        if isinstance(other, AccountDto):
+            return self.__account_id == other.__account_id and self.__name == other.__name and self.__age == other.__age and self.__gender == other.__gender and self.__social_num == other.__social_num and self.__balance == other.__balance
+        return False
+
+    def __hash__(self):
+        """Overrides the default implementation"""
+        return hash(tuple(sorted(self.__dict__.items())))
+
     def __str__(self):
         return f'Account id: {self.__account_id}\n' \
                f'Name: {self.__name}\n' \
                f'Gender: {self.__gender}\n' \
                f'Social Number: {self.__social_num}\n' \
                f'Balance: {self.__balance}\n'
+
+# TODO: Equals method
